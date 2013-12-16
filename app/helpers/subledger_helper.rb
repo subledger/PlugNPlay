9af135e2e5c7a894cd7c182f5372c9373d0c06c4
Alexandre Michetti Manduca <a.michetti@gmail.com>
2013-12-16 21:02:45 -0200

    remove creds from webapp link

--- app/helpers/subledger_helper.rb
+++ app/helpers/subledger_helper.rb
@@ -1,9 +1,6 @@
 module SubledgerHelper
   def subledger_app_link
-    setup = subledger_service.cached_get_setup
-    puts setup
-
-   "<a href='http://subledger.com/app/?key=#{setup['key_id']}&secret=#{setup['secret']}&org=#{setup['org_id']}&book=#{setup['book_id']}' target='_blank'>Open Subledger App</a>".html_safe
+   "<a href='http://subledger.com/app' target='_blank'>Open Subledger App</a>".html_safe
   end
 
 private
