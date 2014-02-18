e3b11123ee30015d66f2759e4ee3c99472d54e08
Alexandre Michetti Manduca <a.michetti@gmail.com>
2014-02-18 17:33:07 -0300

    add caching to app config and mapping

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -5,6 +5,13 @@ class Mapping < ActiveRecord::Base
   validates :key, :value,
             presence: true
 
+  after_commit :flush_cache
+
+  def flush_cache
+    Rails.cache.delete [self.key, "exists"]
+    Rails.cache.delete [self.key, "value"]
+  end
+
   def to_s
     "#{key} = #{value}"
   end
@@ -14,7 +21,11 @@ class Mapping < ActiveRecord::Base
   end
 
   def self.entity_map_exists?(entity, key)
-    Mapping.exists?(key: "#{entity}::#{key}")
+    entity_key = "#{entity}::#{key}"
+
+    Rails.cache.fetch [entity_key, "exists"] do
+      Mapping.exists?(key: entity_key)
+    end
   end
 
   def self.find_entity_map(entity, key)
@@ -22,7 +33,11 @@ class Mapping < ActiveRecord::Base
   end
 
   def self.entity_map_value(entity, key)
-    mapping = find_entity_map(entity, key)
-    mapping.present? ? mapping.value : nil
+    entity_key = "#{entity}::#{key}"
+
+    Rails.cache.fetch [entity_key, "value"] do
+      mapping = find_entity_map(entity, key)
+      mapping.present? ? mapping.value : nil
+    end
   end
 end
