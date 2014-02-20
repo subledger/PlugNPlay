1b08c47ec37e6a8b60500d3cb0f42beb3eb9a974
Alexandre Michetti Manduca <a.michetti@gmail.com>
2014-02-20 15:31:26 -0300

    deployment configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-working_directory "/opt/atpay"
-pid "/opt/atpay/tmp/pids/unicorn.pid"
-stderr_path "/opt/atpay/log/unicorn.log"
-stdout_path "/opt/atpay/log/unicorn.log"
+working_directory "/opt/gateway"
+pid "/opt/gateway/tmp/pids/unicorn.pid"
+stderr_path "/opt/gateway/log/unicorn.log"
+stdout_path "/opt/gateway/log/unicorn.log"
 
-listen "/opt/atpay/tmp/sockets/unicorn.atpay.sock"
+listen "/opt/gateway/tmp/sockets/unicorn.gateway.sock"
 preload_app true
 worker_processes 1
 timeout 30
--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -5,7 +5,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/opt/atpay
+APP_ROOT=/opt/gateway
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
