f1ecffdc7fedc457c87e022d7af75f10c0696a63
Alexandre Michetti Manduca <a.michetti@gmail.com>
2014-02-28 00:22:13 -0300

    correct sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => 'redis://localhost:6379/12', :namespace => 'gateway' }
+  config.redis = { :url => 'redis://localhost:6379/12', :namespace => 'granular' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => 'redis://localhost:6379/12', :namespace => 'gateway' }
+  config.redis = { :url => 'redis://localhost:6379/12', :namespace => 'granular' }
 end
